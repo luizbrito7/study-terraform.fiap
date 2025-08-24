be0adb97bc89f3530a85da1ce99a43eb2e2d19f6
luizbrito7 <luizbrito.x@gmail.com>
2025-08-24 13:15:08 -0300

    fix(setup.sh): enhance backend setup script to include GitHub run ID for unique bucket naming

--- terraform/modules/backend/scripts/setup.sh
+++ terraform/modules/backend/scripts/setup.sh
@@ -24,6 +24,7 @@ check_exit_code() {
 
 # variables
 GH_ACCESS_TOKEN="${1}"
+GITHUB_RUN_ID="${2}"
 
 # auth env
 export GH_TOKEN="$GH_ACCESS_TOKEN"
@@ -35,29 +36,33 @@ if gh variable list --repo "${GITHUB_REPOSITORY}" | grep -q "AWS_BUCKET_TFSTATE_
 else
 	log_warn "Variable AWS_BUCKET_TFSTATE_ID NÃO existe, criando backend remoto..."
 
-	# Terraform commands
+    # random name
+	BUCKET_NAME="bucket-tfstate-${GITHUB_RUN_ID}"
+	log_info "Gerando nome do bucket: $BUCKET_NAME"
+
+	# terraform commands
 	log_info "Inicializando Terraform..."
 	terraform -chdir=./terraform/modules/backend init
 	check_exit_code $? "Terraform init"
 	
 	log_info "Executando Terraform plan..."
-	terraform -chdir=./terraform/modules/backend plan -out tfplan
+	terraform -chdir=./terraform/modules/backend plan -var="bucket_tfstate=$BUCKET_NAME" -out tfplan
 	check_exit_code $? "Terraform plan"
 	
 	log_info "Executando Terraform apply..."
 	terraform -chdir=./terraform/modules/backend apply tfplan
 	check_exit_code $? "Terraform apply"
 
-	# Capture output and create variable
+	# capture output and create variable
 	log_info "Capturando nome do bucket..."
-	BUCKET_NAME=$(terraform -chdir=./terraform/modules/backend output -raw bucket_name)
+	BUCKET_NAME_OUTPUT=$(terraform -chdir=./terraform/modules/backend output -raw bucket_name)
 	check_exit_code $? "Terraform output"
 	
 	log_info "Criando repository variable..."
-	gh variable set AWS_BUCKET_TFSTATE_ID --repo "${GITHUB_REPOSITORY}" --body "$BUCKET_NAME"
+	gh variable set AWS_BUCKET_TFSTATE_ID --repo "${GITHUB_REPOSITORY}" --body "$BUCKET_NAME_OUTPUT"
 	check_exit_code $? "Repository variable creation"
 	
-	echo "AWS_BUCKET_TFSTATE_ID=$BUCKET_NAME" >> $GITHUB_OUTPUT
-	log_success "Backend setup completo! Bucket: $BUCKET_NAME"
+	echo "AWS_BUCKET_TFSTATE_ID=$BUCKET_NAME_OUTPUT" >> $GITHUB_OUTPUT
+	log_success "Backend setup completo! Bucket: $BUCKET_NAME_OUTPUT"
 fi
 
